d1cc4c4bf9bd6247c6a8f39054a5e63a9aeb4fe1
shominees <jompa010@gmail.com>
2018-03-15 11:23:44 +0100

    setting up as maven project

--- src/tech/clusterfunk/game/characters/Hacker.java
+++
@@ -1,7 +0,0 @@
-package tech.clusterfunk.game.characters;
-
-import tech.clusterfunk.game.network.Computer;
-
-public interface Hacker extends Character {
-    void hack(Computer computer);
-}
--- src/tech/clusterfunk/game/characters/NPC.java
+++
@@ -1,6 +0,0 @@
-package tech.clusterfunk.game.characters;
-
-public interface NPC extends Character {
-    int getAwareness();
-    void setAwareness(int awareness);
-}
