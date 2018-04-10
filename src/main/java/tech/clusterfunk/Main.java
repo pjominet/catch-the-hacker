d13193a8860159584535c3536e09dc82e92548de
Patrick <jompa010@gmail.com>
2018-04-10 20:49:19 +0200

    added new constants

--- src/main/java/tech/clusterfunk/Main.java
+++ src/main/java/tech/clusterfunk/Main.java
@@ -5,6 +5,8 @@ import tech.clusterfunk.game.Game;
 public class Main {
 
     public static final String CONFIG_ROOT = "/configs/";
+    public static final int MAX_ALLOWED_ACCESSLEVEL = 5;
+    public static final int SUDO = MAX_ALLOWED_ACCESSLEVEL + 1;
 
     public static void main(String[] args) {
         Game game = new Game();
